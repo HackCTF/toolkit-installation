28b980ebdaa9f244de5f6b7119e54679124367c0
Josué Acevedo (Neomatrix) <josuecevedo@gmail.com>
2024-11-03 17:12:13 -0600

    Update installKubernetes.sh

--- installKubernetes.sh
+++ installKubernetes.sh
@@ -4,4 +4,4 @@ cd ~
 git clone https://github.com/HackCTF/kubespray
 cd kubespray
 python -m pip install -r requirements.txt
-ansible-playbook -i /inventory/sample/inventory.ini cluster.yml -vvvv
+ansible-playbook -i ./inventory/sample/inventory.ini cluster.yml -vvvv
